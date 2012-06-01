049649a2bf2dbb87a79a14a5ca7e0c718444715a
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-06-01 14:40:31 -0500

    Added debootstrap util

---
+++ lib/lxc/utils/debootstrap.rb
@@ -0,0 +1,28 @@
+module LXC::Utils
+  module Debootstrap
+    def self.generate(options={})
+      variant     = options[:variant]     || 'minbase'
+      components  = options[:components]  || 'main,universe'
+      packages    = options[:packages]    || 'dialog,apt,apt-utils,aptitude,gpgv,resolvconf,iproute,inetutils-ping,dhcp3-client,ssh,lsb-release,lxcguest'
+      ubuntu      = options[:ubuntu]      || 'natty'
+      ubuntu_arch = options[:ubuntu_arch] || 'amd64'
+      dir         = options[:dir]         || "/var/cache/debootstrap/#{ubuntu}/rootfs-#{ubuntu_arch}"
+
+      # Make a dir first
+      FileUtils.mkdir_p(dir)
+
+      params = [
+        "--verbose",
+        "--variant=#{variant}",
+        "--components=#{components}",
+        "--include=#{packages}",
+        "--arch=#{ubuntu_arch}",
+        ubuntu,
+        dir
+      ]
+
+      cmd = "debootstrap #{params.join(' ')}"
+      `#{cmd}`
+    end
+  end
+end
