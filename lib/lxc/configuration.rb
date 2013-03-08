137b426cda1855d4dc69654710d4e76c651f8763
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-03-07 21:06:51 -0600

    Add spaces for better readability

--- lib/lxc/configuration.rb
+++ lib/lxc/configuration.rb
@@ -17,9 +17,11 @@ module LXC
     # @return [LXC::Configuration]
     def self.load_file(path)
       fullpath = File.expand_path(path)
+
       if !File.exists?(fullpath)
         raise ArgumentError, "File '#{path}' does not exist."
       end
+
       LXC::Configuration.new(File.read(fullpath))
     end
 
@@ -45,15 +47,20 @@ module LXC
     def save_to_file(path)
       fullpath = File.expand_path(path)
       lines = []
+
       @content.each_pair do |key,value|
         k = "lxc.#{key.gsub('_', '.')}"
+
         if value.kind_of?(Array)
           lines << value.map { |v| "#{k} = #{v}" }
         else
           lines << "#{k} = #{value}"
         end
       end
-      File.open(path, 'w') { |f| f.write(lines.flatten.join("\n")) }
+
+      File.open(path, 'w') do |f|
+        f.write(lines.flatten.join("\n"))
+      end
     end
 
     private
@@ -61,16 +68,24 @@ module LXC
     def parse(data)
       hash = {}
       lines = data.split("\n").map(&:strip).select { |l| !l.empty? && l[0,1] != '#' }
+
       lines.each do |l|
         key,value = l.split('=').map(&:strip)
+
         if !valid_option?(key)
           raise ConfigurationError, "Invalid config attribute: #{key}."
         end
+
         key.gsub!(/^lxc\./, '').gsub!('.', '_')
+        
         hash[key] = [] if !hash.key?(key)
         hash[key] << value
       end
-      hash.each_pair { |k,v| hash[k] = v.first if v.size == 1 }
+
+      hash.each_pair do |k, v| 
+        hash[k] = v.first if v.size == 1
+      end
+
       hash
     end
   end
